7f1f3678b8966b414a491685c711269b249c1c70
Henry Lao <hclao1995@gmail.com>
2022-06-20 06:12:51 -0700

    update

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,13 +4,18 @@ import gzip
 import tarfile
 import pickle
 from extra.utils import fetch
+from pathlib import Path
+import os
+
+fullpath = Path(os.path.realpath(__file__))
+ROOT = Path(os.path.dirname(fullpath))
 
 def fetch_mnist():
   parse = lambda file: np.frombuffer(gzip.open(file).read(), dtype=np.uint8).copy()
-  X_train = parse("datasets/mnist/train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28*28)).astype(np.float32)
-  Y_train = parse("datasets/mnist/train-labels-idx1-ubyte.gz")[8:]
-  X_test = parse("datasets/mnist/t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28*28)).astype(np.float32)
-  Y_test = parse("datasets/mnist/t10k-labels-idx1-ubyte.gz")[8:]
+  X_train = parse(ROOT / "mnist/train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28*28)).astype(np.float32)
+  Y_train = parse(ROOT/"mnist/train-labels-idx1-ubyte.gz")[8:]
+  X_test = parse(ROOT/"mnist/t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28*28)).astype(np.float32)
+  Y_test = parse(ROOT/"mnist/t10k-labels-idx1-ubyte.gz")[8:]
   return X_train, Y_train, X_test, Y_test
 
 def fetch_cifar():
